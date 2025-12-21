7061b838bc46c2f0824b57a65cef77168da983fb
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-12-21 20:57:59 +0100

    setup up files

--- main/views.py
+++ main/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 
 
 def index(request) -> HttpResponse:
-    return HttpResponse("Home page")
+    context: dict = {
+        "title": "Home",
+        "content": "Главная страница магазина - HOME",
+        "list": ["first", "second"],
+        "dict": {"first": 1},
+        "is_authenticated": False,
+    }
+    return render(request, "main/index.html", context)
 
 
 def about(request) -> HttpResponse:
