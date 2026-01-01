cb54612565abf65a1d191b0eaef5235b44b18fac
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-01-01 18:11:35 +0200

    Add base template, about page and update views and urls

--- main/views.py
+++ main/views.py
@@ -1,17 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request) -> HttpResponse:
-    context: dict = {
-        "title": "Home",
-        "content": "Главная страница магазина - HOME",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": False,
+def index(request: HttpRequest) -> HttpResponse:
+    context: dict[str, str] = {
+        "title": "Home — Главная",
+        "content": "Магазин мебели HOME",
     }
     return render(request, "main/index.html", context)
 
 
-def about(request) -> HttpResponse:
-    return HttpResponse("About page")
+def about(request: HttpRequest) -> HttpResponse:
+    context: dict[str, str] = {
+        "title": "Home — О нас",
+        "content": "О нас",
+        "text_on_page": (
+            "Текст о том, почему этот магазин такой классный " "и какой хороший товар."
+        ),
+    }
+    return render(request, "main/about.html", context)
