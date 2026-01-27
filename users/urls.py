6a7a2737c8025909c28d5bf20caa0804188112c3
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-01-27 17:26:27 +0200

    templates for users added

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ app_name = "users"
 
 urlpatterns = [
     path("login/", views.login, name="login"),
-    path("registartion/", views.registartion, name="registartion"),
+    path("registration/", views.registration, name="registration"),
     path("profile/", views.profile, name="profile"),
     path("logout/", views.logout, name="logout"),
 ]
