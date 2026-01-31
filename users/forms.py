25e308122de20c5ee723dd995dcf19378f2db27a
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-01-31 17:23:22 +0100

    fixing profile form m controller and notifications

--- users/forms.py
+++ users/forms.py
@@ -108,3 +108,58 @@ class UserRegistrationForm(UserCreationForm):
     #         }
     #     )
     # )
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+
+    # image = forms.ImageField(
+    #     widget=forms.FileInput(attrs={"class": "form-control mt-3"}), required=False
+    # )
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш email *youremail@example.com",
+    #             # 'readonly': True,
+    #         }
+    #     ),
+    # )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -18,6 +19,7 @@ def login(request):
 
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
@@ -34,19 +36,32 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(
+                request, f"{user.username}, Вы успешно зарегистрировались и вошли в аккаунт"
+            )
             return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
     context: dict[str, str] = {"title": "Home - Регистрация", "form": form}
     return render(request, "users/registration.html", context)
 
-
+@login_required
 def profile(request):
-    context: dict[str, str] = {"title": "Home - Кабинет"}
+    if request.method == "POST":
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профиль успешно обновлён")
+            return HttpResponseRedirect(reverse("user:profile"))
+    else:
+        form = ProfileForm(instance=request.user)
+    context: dict[str, str] = {"title": "Home - Кабинет", "form": form}
     return render(request, "users/profile.html", context)
 
 
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse("main:index"))
-
