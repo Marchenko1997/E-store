2d76dc1e3dcb21781109915785a891c2ebf97e5e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-02-03 20:30:24 +0100

    Sessions - carts for anonymous users

--- carts/views.py
+++ carts/views.py
@@ -13,22 +13,40 @@ def cart_add(request):
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
-
         if carts.exists():
             cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
+            cart.quantity += 1
+            cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        
+        if not request.session.session_key:
+            request.session.create()
+        
+        
+        session_key = request.session.session_key
+        carts = Cart.objects.filter(session_key=session_key, product=product)
+        
+        if carts.exists():
+            cart = carts.first()
+            cart.quantity += 1
+            cart.save()
+        else:
+            Cart.objects.create(
+                session_key=session_key, 
+                product=product, 
+                quantity=1
+            )
 
+    
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_cart}, request=request)
 
     response_data = {
-    "message": "Товар добавлен в корзину",
-    "cart_items_html": cart_items_html
-}
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html
+    }
 
     return JsonResponse(response_data)
 
