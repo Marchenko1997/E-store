0a0e57de6ae64215bf282c12c7434ecef9098a78
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-02-04 19:30:01 +0100

    Admin panel setup

--- carts/models.py
+++ carts/models.py
@@ -31,11 +31,14 @@ class Cart(models.Model):
         db_table = "cart"
         verbose_name: str = "Корзина"
         verbose_name_plural: str = "Корзина"
-    
+
     objects = CartQueryset().as_manager()
-    
+
     def products_price(self):
         return round(self.product.sell_price() * self.quantity, 2)
 
-    def __str__(self) -> str:
-        return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+    def __str__(self):
+        if self.user:
+            return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+
+        return f"Анонимная корзина | Товар {self.product.name} | Количество {self.quantity}"
