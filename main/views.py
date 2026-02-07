32700bdf337de5a034a685b25ab2f22b68771a2e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-02-07 14:44:21 +0100

    updating main and goods controllers

--- main/views.py
+++ main/views.py
@@ -1,25 +1,49 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from goods.models import Categories
+from django.views.generic import TemplateView
 
 
-def index(request: HttpRequest) -> HttpResponse:
+class IndexView(TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - Главная"
+        context["content"] = "Магазин мебели HOME"
+        return context
 
 
-    context: dict[str, str] = {
-        "title": "Home — Главная",
-        "content": "Магазин мебели HOME",
-       
-    }
-    return render(request, "main/index.html", context)
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = (
+            "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        )
+        return context
 
 
-def about(request: HttpRequest) -> HttpResponse:
-    context: dict[str, str] = {
-        "title": "Home — О нас",
-        "content": "О нас",
-        "text_on_page": (
-            "Текст о том, почему этот магазин такой классный " "и какой хороший товар."
-        ),
-    }
-    return render(request, "main/about.html", context)
+# def index(request: HttpRequest) -> HttpResponse:
+
+
+#     context: dict[str, str] = {
+#         "title": "Home — Главная",
+#         "content": "Магазин мебели HOME",
+
+#     }
+#     return render(request, "main/index.html", context)
+
+
+# def about(request: HttpRequest) -> HttpResponse:
+#     context: dict[str, str] = {
+#         "title": "Home — О нас",
+#         "content": "О нас",
+#         "text_on_page": (
+#             "Текст о том, почему этот магазин такой классный " "и какой хороший товар."
+#         ),
+#     }
+#     return render(request, "main/about.html", context)
