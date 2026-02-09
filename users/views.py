9e4ed1c8d290f09a62b1a5bf35bd9965a265b62f
Halyna Marchenko <marchenkohalyna888@gmail.com>
2026-02-09 18:36:34 +0100

    caching

--- users/views.py
+++ users/views.py
@@ -6,8 +6,10 @@ from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
+from django.core.cache import cache
 from django.views.generic import CreateView,  UpdateView, TemplateView
 from carts.models import Cart
+from common.mixins import CacheMixin
 from orders.models import Order, OrderItem
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
@@ -75,7 +77,7 @@ class UserRegistrationView(CreateView):
         return context
 
 
-class UserProfileView(LoginRequiredMixin, UpdateView):
+class UserProfileView(LoginRequiredMixin, CacheMixin, UpdateView):
     template_name = "users/profile.html"
     form_class = ProfileForm
     success_url = reverse_lazy("users:profile")
@@ -93,13 +95,23 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Home - Кабинет'
-        context['orders'] =Order.objects.filter(user=self.request.user).prefetch_related(
+        context["title"] = "Home - Кабинет"
+
+        # Можно вынести сам запрос в отдельный метод этого класса контроллера
+        orders = (
+            Order.objects.filter(user=self.request.user)
+            .prefetch_related(
                 Prefetch(
                     "orderitem_set",
                     queryset=OrderItem.objects.select_related("product"),
                 )
-            ).order_by("-id")
+            )
+            .order_by("-id")
+        )
+
+        context["orders"] = self.set_get_cache(
+            orders, f"user_{self.request.user.id}_orders", 60
+        )
         return context
 
 
